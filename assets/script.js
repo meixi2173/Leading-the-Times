1be166061f1a7830d417517f1fb97e1ad8b14985
meixi2173 <233150568+meixi2173@users.noreply.github.com>
2025-09-23 11:26:14 -0600

    升级到 v2.2：首页门户 + 白皮书/操作协议镶嵌

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+document.getElementById("enterSite").onclick = () => {
+  document.getElementById("portal").style.display = "none";
+  document.getElementById("mainSite").style.display = "block";
+};
